5fe9bd0d00fdd8c4c9c1a45d63068287ce608c17
Gerth123 <robingerth21@gmail.com>
2024-10-10 16:58:10 +0200

    Added the component for the message-area-header

--- src/app/main/main-message-area/main-message-area.component.ts
+++ src/app/main/main-message-area/main-message-area.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import {MatCardModule} from '@angular/material/card';
 
 @Component({
   selector: 'app-main-message-area',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule],
   templateUrl: './main-message-area.component.html',
   styleUrl: './main-message-area.component.scss'
 })
--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MainMessageAreaComponent } from './main-message-area/main-message-area.component';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [],
+  imports: [MainMessageAreaComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
