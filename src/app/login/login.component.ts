f561ab54c0eccd2bf68eb326f3e348b49e0b1538
Andino Eichberger <eichberger.andino@gmail.com>
2024-10-10 05:06:10 +0200

    start hardcode for login und optimize global vars + design basic elements globaly

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule, MatButtonModule, MatFormFieldModule, MatInputModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
-export class LoginComponent {
-
-}
+export class LoginComponent {}
