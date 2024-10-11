aa74efd19be6cccfb384210f9cbf472c7b4103aa
Andino Eichberger <eichberger.andino@gmail.com>
2024-10-11 06:01:57 +0200

    merge with another branche und optimize global design for input, button, icon and optimize login component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,7 @@
 import { Routes } from '@angular/router';
 import { MainComponent } from './main/main.component';
 import { LoginComponent } from './login/login.component';
-import { LoginComponent } from './login/login.component';
-import { MainComponent } from './main/main.component';
 
-export const routes: Routes = [
-  { path: '', component: LoginComponent },
-  { path: 'main', component: MainComponent },
-];
 export const routes: Routes = [
   { path: '', component: LoginComponent },
   { path: 'main', component: MainComponent },
