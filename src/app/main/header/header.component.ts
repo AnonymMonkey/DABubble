667dc08a40aa56855a3712da7a8af5ef19942db1
Semir Muratovic <148802821+SemirMuratovic@users.noreply.github.com>
2024-10-11 22:24:58 +0200

    Profile dialog added

--- src/app/main/header/header.component.ts
+++ src/app/main/header/header.component.ts
@@ -2,6 +2,8 @@ import { Component, ViewEncapsulation } from '@angular/core';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+import { MatDialog } from '@angular/material/dialog';
+import { ProfileDialogComponent } from './profile-dialog/profile-dialog.component';
 
 @Component({
   selector: 'app-header',
@@ -12,5 +14,8 @@ import { MatInputModule } from '@angular/material/input';
   encapsulation: ViewEncapsulation.None,
 })
 export class HeaderComponent {
-  toggleDropdown() {}
+  constructor(public dialog: MatDialog) {}
+  toggleDropdown(): void {
+    const dialogRef = this.dialog.open(ProfileDialogComponent);
+  }
 }
