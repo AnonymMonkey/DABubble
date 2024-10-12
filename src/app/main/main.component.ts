d2805e034f458233de547d182f621483ee25f8f7
Semir Muratovic <148802821+SemirMuratovic@users.noreply.github.com>
2024-10-12 13:29:50 +0200

    Side-nav button added to main

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { MainMessageAreaComponent } from './main-message-area/main-message-area.component';
-
+import { MatIconModule } from '@angular/material/icon';
+// import { MatButtonModule } from '@angular/material/button';
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [HeaderComponent, MainMessageAreaComponent],
+  imports: [HeaderComponent, MainMessageAreaComponent, MatIconModule],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss',
 })
