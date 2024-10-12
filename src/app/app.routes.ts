7da7dfc83300fda23cc0f4ebe2d6682e5a3dfd68
Andino Eichberger <eichberger.andino@gmail.com>
2024-10-12 23:38:03 +0200

    delete landing-page-component and optimize struture and design

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,19 +3,14 @@ import { MainComponent } from './main/main.component';
 import { RegisterComponent } from './landing-page/register/register.component';
 import { ImprintComponent } from './shared/imprint/imprint.component';
 import { PrivacyPolicyComponent } from './shared/privacy-policy/privacy-policy.component';
-import { LandingPageComponent } from './landing-page/landing-page.component';
 import { LoginComponent } from './landing-page/login/login.component';
+import { AppComponent } from './app.component';
 
 export const routes: Routes = [
   // Hier wird die Login-Seite geladen, die Child-Routes wie Login und Register enthält
-  {
-    path: '',
-    component: LandingPageComponent,
-    children: [
-      { path: '', component: LoginComponent }, // Standardmäßig Login anzeigen
-      { path: 'register', component: RegisterComponent }, // Route für Registrierung
-    ],
-  },
+
+  { path: '', component: LoginComponent }, // Standardmäßig Login anzeigen
+  { path: 'register', component: RegisterComponent }, // Route für Registrierung
 
   // Hauptkomponente nach Login (Main-Bereich der App)
   { path: 'main', component: MainComponent },
