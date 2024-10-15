b566da1c5d601aa0a6a9aea701d789830b7e866f
Gerth123 <robingerth21@gmail.com>
2024-10-15 20:47:37 +0200

    Created separate components for the messages.

--- src/app/main/main-message-area/chat-components/own-message-template/own-message-template.component.ts
+++ src/app/main/main-message-area/chat-components/own-message-template/own-message-template.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { MainMessageAreaComponent } from '../../main-message-area.component';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-own-message-template',
   standalone: true,
-  imports: [],
+  imports: [NgClass],
   templateUrl: './own-message-template.component.html',
   styleUrl: './own-message-template.component.scss'
 })
 export class OwnMessageTemplateComponent {
+  isEmojiContainerVisible: number = 0;
 
+  constructor(public mainMessageArea: MainMessageAreaComponent) {}
+  showEmojiContainer(id: number) {
+    this.isEmojiContainerVisible = id;
+  }
+
+  hideEmojiContainer() {
+    this.isEmojiContainerVisible = 0;
+  }
 }
--- src/app/main/main-message-area/message-area-chat-history/message-area-chat-history.component.ts
+++ src/app/main/main-message-area/message-area-chat-history/message-area-chat-history.component.ts
@@ -4,11 +4,13 @@ import { MatIcon } from '@angular/material/icon';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { ThreadComponent } from '../thread/thread.component';
 import { MainMessageAreaComponent } from '../main-message-area.component';
+import { OtherMessageTemplateComponent } from '../chat-components/other-message-template/other-message-template.component';
+import { OwnMessageTemplateComponent } from '../chat-components/own-message-template/own-message-template.component';
 
 @Component({
   selector: 'app-message-area-chat-history',
   standalone: true,
-  imports: [MatIcon, NgClass, NgFor, MatSidenavModule, NgFor, CommonModule, ThreadComponent, MainMessageAreaComponent],
+  imports: [MatIcon, NgClass, NgFor, MatSidenavModule, NgFor, CommonModule, ThreadComponent, MainMessageAreaComponent, OtherMessageTemplateComponent, OwnMessageTemplateComponent],
   templateUrl: './message-area-chat-history.component.html',
   styleUrls: ['./message-area-chat-history.component.scss'],
 })
