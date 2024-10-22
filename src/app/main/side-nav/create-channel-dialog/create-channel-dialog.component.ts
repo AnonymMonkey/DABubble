748eaa31a9d4a9d108ebe619a471e5d9922a4286
Semir Muratovic <148802821+SemirMuratovic@users.noreply.github.com>
2024-10-22 12:39:05 +0200

    Inputs bounded to some variables

--- src/app/main/side-nav/create-channel-dialog/create-channel-dialog.component.ts
+++ src/app/main/side-nav/create-channel-dialog/create-channel-dialog.component.ts
@@ -31,6 +31,7 @@ import { MatInputModule } from '@angular/material/input';
 })
 export class CreateChannelDialogComponent {
   channelName: string = '';
+  description: string = '';
   invalid: boolean = true;
 
   readonly dialogRef = inject(MatDialogRef<CreateChannelDialogComponent>);
@@ -43,5 +44,10 @@ export class CreateChannelDialogComponent {
     } else {
       this.invalid = false;
     }
+  }
+
+  test() {
+    console.log(this.channelName);
+    console.log(this.description);
   }
 }
