3e86c88caf3f2d420edcb7555ccd6406abac40d8
Semir Muratovic <148802821+SemirMuratovic@users.noreply.github.com>
2024-10-23 21:58:39 +0200

    Chips with autocomplete dummy implemented

--- src/app/main/side-nav/add-users-to-new-channel-dialog/add-users-to-new-channel-dialog.component.ts
+++ src/app/main/side-nav/add-users-to-new-channel-dialog/add-users-to-new-channel-dialog.component.ts
@@ -1,26 +1,18 @@
-import { CommonModule } from '@angular/common';
-import {
-  Component,
-  inject,
-  ChangeDetectionStrategy,
-  signal,
-} from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { AsyncPipe, CommonModule } from '@angular/common';
+import { Component, computed, inject, model, signal } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatDialogModule, MatDialogRef } from '@angular/material/dialog';
 import { MatIconModule } from '@angular/material/icon';
 import { MatRadioModule } from '@angular/material/radio';
 import { LiveAnnouncer } from '@angular/cdk/a11y';
 import { COMMA, ENTER } from '@angular/cdk/keycodes';
-import {
-  MatChipEditedEvent,
-  MatChipInputEvent,
-  MatChipsModule,
-} from '@angular/material/chips';
+import { MatChipInputEvent, MatChipsModule } from '@angular/material/chips';
 import { MatFormFieldModule } from '@angular/material/form-field';
-
-export interface Fruit {
-  name: string;
-}
+import { MatInputModule } from '@angular/material/input';
+import {
+  MatAutocompleteModule,
+  MatAutocompleteSelectedEvent,
+} from '@angular/material/autocomplete';
 
 @Component({
   selector: 'app-add-users-to-new-channel-dialog',
@@ -33,6 +25,10 @@ export interface Fruit {
     FormsModule,
     MatFormFieldModule,
     MatChipsModule,
+    MatInputModule,
+    MatAutocompleteModule,
+    ReactiveFormsModule,
+    AsyncPipe,
   ],
   templateUrl: './add-users-to-new-channel-dialog.component.html',
   styleUrl: './add-users-to-new-channel-dialog.component.scss',
@@ -54,13 +50,27 @@ export class AddUsersToNewChannelDialogComponent {
     }
   }
 
-  readonly addOnBlur = true;
-  readonly separatorKeysCodes = [ENTER, COMMA] as const;
-  readonly fruits = signal<Fruit[]>([
-    { name: 'Lemon' },
-    { name: 'Lime' },
-    { name: 'Apple' },
-  ]);
+  //NOTE - Hier werden die chips angezeigt
+
+  readonly separatorKeysCodes: number[] = [ENTER, COMMA];
+  readonly currentFruit = model('');
+  readonly fruits = signal(['Lemon']);
+  readonly allFruits: string[] = [
+    'Apple',
+    'Lemon',
+    'Lime',
+    'Orange',
+    'Strawberry',
+  ];
+  readonly filteredFruits = computed(() => {
+    const currentFruit = this.currentFruit().toLowerCase();
+    return currentFruit
+      ? this.allFruits.filter((fruit) =>
+          fruit.toLowerCase().includes(currentFruit)
+        )
+      : this.allFruits.slice();
+  });
+
   readonly announcer = inject(LiveAnnouncer);
 
   add(event: MatChipInputEvent): void {
@@ -68,14 +78,14 @@ export class AddUsersToNewChannelDialogComponent {
 
     // Add our fruit
     if (value) {
-      this.fruits.update((fruits) => [...fruits, { name: value }]);
+      this.fruits.update((fruits) => [...fruits, value]);
     }
 
     // Clear the input value
-    event.chipInput!.clear();
+    this.currentFruit.set('');
   }
 
-  remove(fruit: Fruit): void {
+  remove(fruit: string): void {
     this.fruits.update((fruits) => {
       const index = fruits.indexOf(fruit);
       if (index < 0) {
@@ -83,28 +93,14 @@ export class AddUsersToNewChannelDialogComponent {
       }
 
       fruits.splice(index, 1);
-      this.announcer.announce(`Removed ${fruit.name}`);
+      this.announcer.announce(`Removed ${fruit}`);
       return [...fruits];
     });
   }
 
-  edit(fruit: Fruit, event: MatChipEditedEvent) {
-    const value = event.value.trim();
-
-    // Remove fruit if it no longer has a name
-    if (!value) {
-      this.remove(fruit);
-      return;
-    }
-
-    // Edit existing fruit
-    this.fruits.update((fruits) => {
-      const index = fruits.indexOf(fruit);
-      if (index >= 0) {
-        fruits[index].name = value;
-        return [...fruits];
-      }
-      return fruits;
-    });
+  selected(event: MatAutocompleteSelectedEvent): void {
+    this.fruits.update((fruits) => [...fruits, event.option.viewValue]);
+    this.currentFruit.set('');
+    event.option.deselect();
   }
 }
