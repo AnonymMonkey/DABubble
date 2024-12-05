d63c760acd4c647386681607a32ae69c41486caf
Andino Eichberger <eichberger.andino@gmail.com>
2024-12-05 03:17:01 +0100

    visualisize imprint

--- src/app/shared/components/imprint/imprint.component.ts
+++ src/app/shared/components/imprint/imprint.component.ts
@@ -1,12 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { RoutingService } from '../../services/routing-service/routing.service';
 
 @Component({
   selector: 'app-imprint',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, MatIconModule, CommonModule],
   templateUrl: './imprint.component.html',
-  styleUrl: './imprint.component.scss'
+  styleUrl: './imprint.component.scss',
 })
 export class ImprintComponent {
+  constructor(private routingService: RoutingService) {}
 
+  navigateToLogin() {
+    this.routingService.navigateToLogin();
+  }
 }
